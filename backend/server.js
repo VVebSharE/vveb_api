2f45b03b963a36eefa6ca236569c815aa4152c16
VVebSharE <78077715+VVebSharE@users.noreply.github.com>
2023-01-03 13:04:22 +0530

    cors error resolved for authorization

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Origin");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 })
 
